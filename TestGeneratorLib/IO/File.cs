8c182002ab2e5757dda25fa2ea0e9526c63cef54
artem0189 <56150204+artem0189@users.noreply.github.com>
2020-10-17 19:47:58 +0300

    Add File class & Create pipiline structure

---
+++ TestGeneratorLib/IO/File.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGeneratorLib.IO
+{
+    internal class File
+    {
+        internal string FileName { get; }
+        internal string FolderPath { get; }
+        internal string FileContent { get; set; }
+
+        internal File(string folderPath, string filePath)
+        {
+            FolderPath = folderPath;
+            FileName = "TestTemplate" + Path.GetFileName(filePath);
+        }
+
+        internal async Task Read(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                FileContent = await reader.ReadToEndAsync();
+            }
+        }
+
+        internal async Task Write()
+        {
+            using (StreamWriter writer = new StreamWriter(FolderPath + "/" + FileName))
+            {
+                await writer.WriteAsync(FileContent);
+            }
+        }
+    }
+}
--- TestGeneratorLib/Pipeline.cs
+++ TestGeneratorLib/Pipeline.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
+using TestGeneratorLib.IO;
 
 namespace TestGeneratorLib
 {
     internal class Pipeline
     {
+        private string FileFolder { get; }
         private int FilesInputCount { get; }
         private int FilesOutputCount { get; }
         private int TasksCount { get; }
@@ -18,16 +20,54 @@ namespace TestGeneratorLib
             TasksCount = tasksCount;
         }
 
-        internal async Task Processing(List<string> filesName)
+        internal async Task Processing(List<string> filesPath)
         {
+            var readingBlock = new TransformBlock<string, File>(
+                async filePath => await ReadFile(filePath),
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = FilesInputCount
+                });
+            var generatingBlock = new TransformBlock<File, File>(
+                file => GenerateTest(file),
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = TasksCount
+                });
+            var writingBlock = new ActionBlock<File>(
+                file => WriteFile(file),
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = FilesOutputCount
+                });
 
+            readingBlock.LinkTo(generatingBlock);
+            generatingBlock.LinkTo(writingBlock);
+
+            for (int i = 0; i < filesPath.Count; i++)
+            {
+                readingBlock.Post(filesPath[i]);
+            }
+            readingBlock.Complete();
+
+            await writingBlock.Completion;
         }
 
-        private async Task<string> ReadFile(string fileName)
+        private async Task<File> ReadFile(string filePath)
         {
-
+            File file = new File(FileFolder, filePath);
+            await file.Read(filePath);
+            return file;
         }
 
-        private async Task<>
+        private File GenerateTest(File file)
+        {
+            return file;
+        }
+
+        private async Task WriteFile(File file)
+        {
+            await file.Write();
+        }
     }
 }
